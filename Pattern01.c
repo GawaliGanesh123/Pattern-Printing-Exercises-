e94a1a6d60805be5c2dd28c21d3cd7ed5ced9577
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-06-11 11:43:27 +0530

    Add files via upload

---
+++ Pattern01.c
@@ -0,0 +1,33 @@
+/*
+  Accept one number from user and print that number of star on screen.
+
+   Input- 5
+   Output- * * * * *
+
+*/
+
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+	 int iCnt=0;
+
+	 for(iCnt=1; iCnt<=iNo; iCnt++)
+	 {
+	    printf("*\t");
+	 }
+}
+
+
+int main()
+{
+    int iValue=0;
+
+    printf("Enter the Number:\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+	return 0;
+}
