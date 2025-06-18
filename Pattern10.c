c772868565d7333035e18d5783a5a32a9c08ac22
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-06-18 11:56:04 +0530

    Add files via upload

---
+++ Pattern10.c
@@ -0,0 +1,65 @@
+/* Accept number of rows and number of columns from user and display below pattern.
+
+Input : iRow = 4 iCol = 4
+
+Output : A 	 B 	 C 	  D
+         a 	 b 	 c 	  d
+         A   B   C    D
+         a   b   c    d
+
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void DisplayPattern(int iRow, int iCol)
+{
+	int i=0; 
+	int j=0;
+	char ch1='A';
+	char ch2='a';
+
+	if(iRow != iCol)
+	{
+		printf("Please Enter Equal  Rows & Columns Count");
+		return;
+	}
+
+
+	for(i=1; i<=iRow; i++)
+	{
+		  ch1='A';
+		  ch2='a';
+		  
+       for(j=1; j<=iCol; j++)
+       {
+         if(i % 2 == 0)
+         	printf("%c\t",ch2);
+         else
+         	printf("%c\t",ch1);
+
+         ch1++;
+         ch2++;
+       }
+        printf("\n");
+	}
+
+}
+
+
+int main()
+{
+	int iValue1=0; 
+	int iValue2=0;
+
+	printf("Enter number of rows:\n");
+	scanf("%d",&iValue1);
+
+	printf("Enter the number of columns:\n");
+	scanf("%d",&iValue2);
+
+	DisplayPattern(iValue1, iValue2);
+
+	return 0;
+}
