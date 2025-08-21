e984e54b525c026a002f6f9949d03b3a1b2840b4
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-08-21 22:05:42 +0530

    Update Pattern28.c

--- Pattern28.c
+++ Pattern28.c
@@ -15,7 +15,7 @@
  
 void Display_Pattern(int iRow, int iCol)
 { 
-        int i=0,j=0;  
+        int i=0,j=0;   
  
         if(iRow != iCol)
         {
@@ -53,6 +53,7 @@ int main()
         return 0;
 
 }
+
 
 
 
