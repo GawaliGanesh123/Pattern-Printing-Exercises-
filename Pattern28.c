1e07d09949676727f1f81e0ec2fb7444c1e3a11e
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-08-30 19:30:41 +0530

    Pattern28.c

--- Pattern28.c
+++ Pattern28.c
@@ -28,7 +28,7 @@ void Display_Pattern(int iRow, int iCol)
           for(j=1; j<=iCol; j++)
           {
             if(i<=j) 
-            {
+            { 
                 printf("*\t");
             }   
            else
