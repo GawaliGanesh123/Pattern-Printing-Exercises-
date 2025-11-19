3f8e97960fe64015a00913d537d736ae264b19cc
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-11-19 09:57:12 +0530

    Fix formatting

--- Pattern14.c
+++ Pattern14.c
@@ -1,8 +1,8 @@
 /* Accept number of rows and number of columns from user and display below pattern.
 
 Input : iRow = 5 iCol = 5
-
-Output :         a 	 b 	 c 	 d 	 e
+ 
+Output : a 	 b 	 c 	 d 	 e
 		 1 	 2 	 3 	 4 	 5
 		 a 	 b 	 c 	 d 	 e
  		 1 	 2 	 3 	 4 	 5
@@ -55,4 +55,5 @@ int main()
 
 	return 0;
 }
+
 
